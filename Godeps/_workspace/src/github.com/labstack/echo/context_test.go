1d192a8b270ec64865293421c337e9b612204342
holys <chendahui007@gmail.com>
2015-11-20 18:31:17 +0800

    add godep

---
+++ Godeps/_workspace/src/github.com/labstack/echo/context_test.go
@@ -0,0 +1,220 @@
+package echo
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+
+	"strings"
+
+	"encoding/xml"
+	"net/url"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type (
+	Template struct {
+		templates *template.Template
+	}
+)
+
+func (t *Template) Render(w io.Writer, name string, data interface{}) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func TestContext(t *testing.T) {
+	userJSON := `{"id":"1","name":"Joe"}`
+	userXML := `<user><id>1</id><name>Joe</name></user>`
+
+	req, _ := http.NewRequest(POST, "/", strings.NewReader(userJSON))
+	rec := httptest.NewRecorder()
+	c := NewContext(req, NewResponse(rec), New())
+
+	// Request
+	assert.NotNil(t, c.Request())
+
+	// Response
+	assert.NotNil(t, c.Response())
+
+	// Socket
+	assert.Nil(t, c.Socket())
+
+	// Param by id
+	c.pnames = []string{"id"}
+	c.pvalues = []string{"1"}
+	assert.Equal(t, "1", c.P(0))
+
+	// Param by name
+	assert.Equal(t, "1", c.Param("id"))
+
+	// Store
+	c.Set("user", "Joe")
+	assert.Equal(t, "Joe", c.Get("user"))
+
+	//------
+	// Bind
+	//------
+
+	// JSON
+	testBind(t, c, "application/json")
+
+	// XML
+	c.request, _ = http.NewRequest(POST, "/", strings.NewReader(userXML))
+	testBind(t, c, ApplicationXML)
+
+	// Unsupported
+	testBind(t, c, "")
+
+	//--------
+	// Render
+	//--------
+
+	tpl := &Template{
+		templates: template.Must(template.New("hello").Parse("Hello, {{.}}!")),
+	}
+	c.echo.SetRenderer(tpl)
+	err := c.Render(http.StatusOK, "hello", "Joe")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Hello, Joe!", rec.Body.String())
+	}
+
+	c.echo.renderer = nil
+	err = c.Render(http.StatusOK, "hello", "Joe")
+	assert.Error(t, err)
+
+	// JSON
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.JSON(http.StatusOK, user{"1", "Joe"})
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, ApplicationJSONCharsetUTF8, rec.Header().Get(ContentType))
+		assert.Equal(t, userJSON, rec.Body.String())
+	}
+
+	// JSONP
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	callback := "callback"
+	err = c.JSONP(http.StatusOK, callback, user{"1", "Joe"})
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, ApplicationJavaScriptCharsetUTF8, rec.Header().Get(ContentType))
+		assert.Equal(t, callback+"("+userJSON+");", rec.Body.String())
+	}
+
+	// XML
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.XML(http.StatusOK, user{"1", "Joe"})
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, ApplicationXMLCharsetUTF8, rec.Header().Get(ContentType))
+		assert.Equal(t, xml.Header, xml.Header, rec.Body.String())
+	}
+
+	// String
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.String(http.StatusOK, "Hello, World!")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, TextPlain, rec.Header().Get(ContentType))
+		assert.Equal(t, "Hello, World!", rec.Body.String())
+	}
+
+	// HTML
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.HTML(http.StatusOK, "Hello, <strong>World!</strong>")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, TextHTMLCharsetUTF8, rec.Header().Get(ContentType))
+		assert.Equal(t, "Hello, <strong>World!</strong>", rec.Body.String())
+	}
+
+	// File
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.File("test/fixture/walle.png", "", false)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, 219885, rec.Body.Len())
+	}
+
+	// File as attachment
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	err = c.File("test/fixture/walle.png", "WALLE.PNG", true)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, rec.Header().Get(ContentDisposition), "attachment; filename=WALLE.PNG")
+		assert.Equal(t, 219885, rec.Body.Len())
+	}
+
+	// NoContent
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	c.NoContent(http.StatusOK)
+	assert.Equal(t, http.StatusOK, c.response.status)
+
+	// Redirect
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	assert.Equal(t, nil, c.Redirect(http.StatusMovedPermanently, "http://labstack.github.io/echo"))
+
+	// Error
+	rec = httptest.NewRecorder()
+	c = NewContext(req, NewResponse(rec), New())
+	c.Error(errors.New("error"))
+	assert.Equal(t, http.StatusInternalServerError, c.response.status)
+
+	// reset
+	c.reset(req, NewResponse(httptest.NewRecorder()), New())
+}
+
+func TestContextQuery(t *testing.T) {
+	q := make(url.Values)
+	q.Set("name", "joe")
+	q.Set("email", "joe@labstack.com")
+
+	req, err := http.NewRequest(GET, "/", nil)
+	assert.NoError(t, err)
+	req.URL.RawQuery = q.Encode()
+
+	c := NewContext(req, nil, New())
+	assert.Equal(t, "joe", c.Query("name"))
+	assert.Equal(t, "joe@labstack.com", c.Query("email"))
+
+}
+
+func TestContextForm(t *testing.T) {
+	f := make(url.Values)
+	f.Set("name", "joe")
+	f.Set("email", "joe@labstack.com")
+
+	req, err := http.NewRequest(POST, "/", strings.NewReader(f.Encode()))
+	assert.NoError(t, err)
+	req.Header.Add(ContentType, ApplicationForm)
+
+	c := NewContext(req, nil, New())
+	assert.Equal(t, "joe", c.Form("name"))
+	assert.Equal(t, "joe@labstack.com", c.Form("email"))
+}
+
+func testBind(t *testing.T, c *Context, ct string) {
+	c.request.Header.Set(ContentType, ct)
+	u := new(user)
+	err := c.Bind(u)
+	if ct == "" {
+		assert.Error(t, UnsupportedMediaType)
+	} else if assert.NoError(t, err) {
+		assert.Equal(t, "1", u.ID)
+		assert.Equal(t, "Joe", u.Name)
+	}
+}
---
+++ Godeps/_workspace/src/github.com/labstack/echo/middleware/logger_test.go
@@ -0,0 +1,86 @@
+package middleware
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogger(t *testing.T) {
+	// Note: Just for the test coverage, not a real test.
+	e := echo.New()
+	req, _ := http.NewRequest(echo.GET, "/", nil)
+	rec := httptest.NewRecorder()
+	c := echo.NewContext(req, echo.NewResponse(rec), e)
+
+	// Status 2xx
+	h := func(c *echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+	Logger()(h)(c)
+
+	// Status 3xx
+	rec = httptest.NewRecorder()
+	c = echo.NewContext(req, echo.NewResponse(rec), e)
+	h = func(c *echo.Context) error {
+		return c.String(http.StatusTemporaryRedirect, "test")
+	}
+	Logger()(h)(c)
+
+	// Status 4xx
+	rec = httptest.NewRecorder()
+	c = echo.NewContext(req, echo.NewResponse(rec), e)
+	h = func(c *echo.Context) error {
+		return c.String(http.StatusNotFound, "test")
+	}
+	Logger()(h)(c)
+
+	// Status 5xx with empty path
+	req, _ = http.NewRequest(echo.GET, "", nil)
+	rec = httptest.NewRecorder()
+	c = echo.NewContext(req, echo.NewResponse(rec), e)
+	h = func(c *echo.Context) error {
+		return errors.New("error")
+	}
+	Logger()(h)(c)
+}
+
+func TestLoggerIPAddress(t *testing.T) {
+	buf := &bytes.Buffer{}
+	log.SetOutput(buf)
+
+	ip := "127.0.0.1"
+
+	e := echo.New()
+	req, _ := http.NewRequest(echo.GET, "/", nil)
+	rec := httptest.NewRecorder()
+	c := echo.NewContext(req, echo.NewResponse(rec), e)
+	h := func(c *echo.Context) error {
+		return c.String(http.StatusOK, "test")
+	}
+
+	mw := Logger()
+
+	// With X-Real-IP
+	req.Header.Add(echo.XRealIP, ip)
+	mw(h)(c)
+	assert.Contains(t, buf.String(), ip)
+
+	// With X-Forwarded-For
+	buf.Reset()
+	req.Header.Del(echo.XRealIP)
+	req.Header.Add(echo.XForwardedFor, ip)
+	mw(h)(c)
+	assert.Contains(t, buf.String(), ip)
+
+	// with req.RemoteAddr
+	buf.Reset()
+	mw(h)(c)
+	assert.Contains(t, buf.String(), ip)
+}
